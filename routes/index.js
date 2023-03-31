5d2b158acf0c34731b6ff4ee5b763945b92a2afa
ABDUL AZIZ <abaz.my.id@gmail.com>
2023-03-31 14:13:18 +0700

    add login

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+const path = require('path');
+const { isLoggedIn, Response } = require('../helpers/util')
+
 
 /* GET home page. */
 module.exports = function (db) {
@@ -10,47 +17,52 @@ module.exports = function (db) {
     try {
       const { input_user, password } = req.body
       var data
-      db.query('SELECT * FROM users WHERE email_user = $1', [input_user], (err, rows) => {
+
+      db.query(`SELECT * FROM users WHERE email_user = ?`, [input_user], (err, rows) => {
         if (err) {
           throw new Error(err)
         }
         data = rows
-        if (rows.rows.length == 0) {
+        if (rows.length == 0) {
           //check username
           console.log('check username')
-          db.query('SELECT * FROM users WHERE username = $1', [input_user], (err, rows2) => {
+          db.query('SELECT * FROM users WHERE username = ?', [input_user], (err, rows2) => {
             if (err) {
               throw new Error(err)
             }
-           
-            if (rows2.rows.length == 0) {
+
+            if (rows2.length == 0) {
               return res.json(new Response({ message: "unregistered e-mail/username" }, false))
             }
             data = rows2
-            //console.log("data", data, rows2)
-            bcrypt.compare(password, data.rows[0].password, async function (err, result) {
+            bcrypt.compare(password, data[0].password, async function (err, result) {
               if (err) throw new Error(err)
-  
+
               if (!result) {
                 return res.json(new Response({ message: "password doesn't match" }, false))
               }
               //crete token 
               var token = jwt.sign({
-                userid: data.rows[0].id_user,
-                email: data.rows[0].email_user,
+                userid: data[0].id_user,
+                email: data[0].email_user,
               }, process.env.SECRETKEY);
-              const { rows } = await db.query(`WITH updated AS (UPDATE public.users SET token = $1 WHERE id_user = $2 RETURNING *) SELECT * FROM updated LEFT JOIN regions ON updated.id_region = regions.id_region;`, [token, data.rows[0].id_user])
-              // console.log("data", rows)
-              res.json(new Response({
-                userid: rows[0].id_user,
-                email: rows[0].email_user,
-                username: rows[0].username,
-                role: rows[0].role,
-  
-                id_region: rows[0].id_region,
-                nama_region: rows[0].nama_region,
-                token: rows[0].token
-              }))
+              db.query(`UPDATE users SET token = ? WHERE id_user = ?;`, [token, data[0].id_user], (err, result) => {
+                if (err) res.status(500).json(new Response(err.toString(), false))
+                db.query(`SELECT * FROM users WHERE id_user = ?;`, [data[0].id_user], (err, result) => {
+                  if (err) res.status(500).json(new Response(err.toString(), false))
+                  const rows = result
+                  res.json(new Response({
+                    userid: rows[0].id_user,
+                    email: rows[0].email_user,
+                    username: rows[0].username,
+                    role: rows[0].role,
+
+                    id_region: rows[0].id_region,
+                    nama_region: rows[0].nama_region,
+                    token: rows[0].token
+                  }))
+                })
+              })
             });
           })
         } else {
@@ -66,18 +78,23 @@ module.exports = function (db) {
               userid: data.rows[0].id_user,
               email: data.rows[0].email_user,
             }, process.env.SECRETKEY);
-            const { rows } = await db.query(`WITH updated AS (UPDATE public.users SET token = $1 WHERE id_user = $2 RETURNING *) SELECT * FROM updated LEFT JOIN regions ON updated.id_region = regions.id_region;`, [token, data.rows[0].id_user])
-            // console.log("data", rows)
-            res.json(new Response({
-              userid: rows[0].id_user,
-              email: rows[0].email_user,
-              username: rows[0].username,
-              role: rows[0].role,
+            db.query(`UPDATE users SET token = ? WHERE id_user = ?;`, [token, data[0].id_user], (err, result) => {
+              if (err) res.status(500).json(new Response(err.toString(), false))
+              db.query(`SELECT * FROM users WHERE id_user = ?;`, [data[0].id_user], (err, result) => {
+                if (err) res.status(500).json(new Response(err.toString(), false))
+                const rows = data.rows
+                res.json(new Response({
+                  userid: rows[0].id_user,
+                  email: rows[0].email_user,
+                  username: rows[0].username,
+                  role: rows[0].role,
 
-              id_region: rows[0].id_region,
-              nama_region: rows[0].nama_region,
-              token: rows[0].token
-            }))
+                  id_region: rows[0].id_region,
+                  nama_region: rows[0].nama_region,
+                  token: rows[0].token
+                }))
+              })
+            })
           });
         }
       })
@@ -92,16 +109,20 @@ module.exports = function (db) {
       const pureToken = token.split(' ')[1]
       try {
         const result = jwt.verify(pureToken, process.env.SECRETKEY)
-        const { rows } = await db.query(`SELECT * FROM public.users WHERE id_user = ${result.userid} ORDER BY id_user ASC`)
-        const user = rows[0]
-        var tokenNow = null
-        const { data } = await db.query(`UPDATE public.users SET token = $1 WHERE id_user = $2 RETURNING *;`, [tokenNow, user.id_user])
-        res.json(new Response({ message: "sign out success" }, true))
+        db.query(`SELECT * FROM users WHERE id_user = ${result.userid} ORDER BY id_user ASC`, (err, data) => {
+          if (err) res.status(500).json(new Response(err.toString(), false))
+          const user = data[0]
+          var tokenNow = null
+          db.query(`UPDATE users SET token = ? WHERE id_user = ?;`, [tokenNow, user.id_user], (err, rows) => {
+            if (err) res.status(500).json(new Response(err.toString(), false))
+            res.json(new Response({ message: "sign out success" }, true))
+          })
+        });
       } catch (e) {
-        res.json(new Response({ message: 'token invalid' }, false))
+        res.json(new Response({ message: e + 'token invalid' }, false))
       }
     } else {
-      res.json(new Response({ message: 'token invalid' }, false))
+      res.json(new Response({ message: 'token invalid gak ada token' }, false))
     }
   });
   return router;
